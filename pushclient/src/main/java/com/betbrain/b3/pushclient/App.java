73c894a0ac606f56c2512a453703a7f164da5029
Huy Banh <huy.banh@gmail.com>
2016-04-15 12:01:14 +0700

    formatted code

--- pushclient/src/main/java/com/betbrain/b3/pushclient/App.java
+++ pushclient/src/main/java/com/betbrain/b3/pushclient/App.java
@@ -4,10 +4,8 @@ package com.betbrain.b3.pushclient;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello B3!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello B3!");
+	}
 }
