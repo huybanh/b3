4040b50f51bfad66307a07dc1ce681259a3300db
Huy Banh <huy.banh@gmail.com>
2016-04-19 09:06:14 +0700

    pull skeleton
    maven config, log4j config, connector & listener skeletons

--- pushclient/src/main/java/com/betbrain/b3/pushclient/App.java
+++ pushclient/src/main/java/com/betbrain/b3/pushclient/App.java
@@ -1,5 +1,8 @@
 package com.betbrain.b3.pushclient;
 
+import com.betbrain.sepc.connector.sdql.SEPCConnector;
+import com.betbrain.sepc.connector.sdql.SEPCPushConnector;
+
 /**
  * Hello world!
  *
@@ -7,5 +10,9 @@ package com.betbrain.b3.pushclient;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello B3!");
+		SEPCConnector pushConnector = new SEPCPushConnector("sept.betbrain.com", 7000);
+		pushConnector.addConnectorListener(new PushListener());
+		pushConnector.start("OddsHistory");
+
 	}
 }
