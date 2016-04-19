1d17fc9bb15424600f2aed2fab9444649fe2ae3d
Huy Banh <huy.banh@gmail.com>
2016-04-19 11:42:37 +0700

    added event_selection document

--- pushclient/src/main/java/com/betbrain/b3/pushclient/PushListener.java
+++ pushclient/src/main/java/com/betbrain/b3/pushclient/PushListener.java
@@ -6,6 +6,9 @@ import com.betbrain.sepc.connector.sdql.SEPCConnectorListener;
 import com.betbrain.sepc.connector.sportsmodel.Entity;
 import com.betbrain.sepc.connector.sportsmodel.EntityChange;
 import com.betbrain.sepc.connector.sportsmodel.EntityChangeBatch;
+import com.betbrain.sepc.connector.sportsmodel.EntityCreate;
+import com.betbrain.sepc.connector.sportsmodel.EntityDelete;
+import com.betbrain.sepc.connector.sportsmodel.EntityUpdate;
 
 public class PushListener implements SEPCConnectorListener {
 	
@@ -17,16 +20,33 @@ public class PushListener implements SEPCConnectorListener {
 			return;
 		}
 		System.out.println("notifyEntityUpdates: " + changeBatch);
-		System.out.println("notifyEntityUpdates: batch id: " + changeBatch.getId());
-		first = true;
-		for (EntityChange ec: changeBatch.getEntityChanges()) {
-			System.out.println("Changed: " + ec);
-			//ec.
+		//System.out.println("notifyEntityUpdates: batch id: " + changeBatch.getId());
+		//first = true;
+		for (EntityChange change: changeBatch.getEntityChanges()) {
+			System.out.println("Changed: " + change);
+			//ec could be: EntityCreate/EntityUpdate/EntityDelete
+			if (change instanceof EntityCreate) {
+				Entity e = ((EntityCreate) change).getEntity();
+				//e.getId();
+				//e.getName();
+				System.out.println("Changed entity: " + e);
+			} else if (change instanceof EntityUpdate) {
+				//Entity e = ((EntityUpdate) change).getEntityId();
+			} else if (change instanceof EntityDelete) {
+				long id = ((EntityDelete) change).getEntityId();
+				//System.out.println("Changed entity: " + e);
+			}
 		}
 	}
 
 	public void notifyInitialDump(List<? extends Entity> entityList) {
 		System.out.println("notifyInitialDump: " + entityList.size());
+		//run1: Read batch [1] containing 81 entities. [153 batches left]
+		//run2: Read batch [1] containing 81 entities. [153 batches left]
+		for (Entity e : entityList) {
+			e.getName();
+			e.getId();
+		}
 	}
 
 }
