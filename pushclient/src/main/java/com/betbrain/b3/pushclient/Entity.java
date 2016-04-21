1439e9d9d1460b9e489572e048dba3d2c9311876
namhnt <haunam85@gmail.com>
2016-04-21 10:56:56 +0700

    Testing More with Generic Ser and De Successfully with Our Event and Entity Class. We can serialize and Deserialize applied polimorphism
    With Entity from bettrain, we could not do it ? still investigate why is
    it

---
+++ pushclient/src/main/java/com/betbrain/b3/pushclient/Entity.java
@@ -0,0 +1,13 @@
+package com.betbrain.b3.pushclient;
+
+public class Entity {
+	private long _Id;
+
+	public long get_Id() {
+		return _Id;
+	}
+
+	public void set_Id(long _Id) {
+		this._Id = _Id;
+	}
+}
