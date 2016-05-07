1d18c1050604d33328890190b36e5abc93d49258
Huy Banh <huy.banh@gmail.com>
2016-05-07 19:17:07 +0700

    fixed test errors

--- pushclient/src/main/java/com/betbrain/b3/pushclient/HashKeyTest.java
+++ pushclient/src/main/java/com/betbrain/b3/pushclient/HashKeyTest.java
@@ -3,7 +3,7 @@ package com.betbrain.b3.pushclient;
 public class HashKeyTest {
 
 	public static void main(String[] args) {
-		System.out.println(BatchWorker.generateHashKey(26473973523L));
+		System.out.println(BatchWorker.generateChangeBatchHashKey(26473973523L));
 	}
 
 }
