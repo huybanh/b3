2a3e65ddcb17fc01eca4e3c177819945fce3cba7
Huy Banh <huy.banh@gmail.com>
2016-05-08 18:13:15 +0700

    queries

--- pushclient/src/main/java/com/betbrain/b3/report/EventQuery.java
+++ pushclient/src/main/java/com/betbrain/b3/report/EventQuery.java
@@ -18,24 +18,34 @@ public class EventQuery {
 
 		//query(219387861);
 		//query(219501132);
-		query(206795928);
+		query(IDs.EVENT_PREMIERLEAGUE);
 	}
 	
 	private static void query(long eventId) {
 
 		JsonMapper jsonMapper = new JsonMapper();
 		
-		//event
+		//league
 		B3KeyEntity keyEntity = new B3KeyEntity(Event.class, eventId);
-		Event event = keyEntity.load(jsonMapper);
+		Event league = keyEntity.load(jsonMapper);
 		
-		//outcomes
-		B3KeyLink keyLink = new B3KeyLink(Event.class, eventId, Outcome.class, "eventId");
-		ArrayList<Long> outcomeIds = keyLink.listLinks();
-		B3KeyEntity.load(jsonMapper, Outcome.class, outcomeIds);
+		//matches
+		B3KeyLink keyLink = new B3KeyLink(league, Event.class, Event.PROPERTY_NAME_parentId);
+		ArrayList<Long> matchIds = keyLink.listLinks();
+		System.out.println(matchIds);
+		
+		for (Long oneId : matchIds) {
+			//keyEntity = new B3KeyEntity(Event.class, matchIds.get(1));
+			//Event match = keyEntity.load(jsonMapper);
+		
+			//outcomes
+			keyLink = new B3KeyLink(Event.class, oneId, Outcome.class, "eventId");
+			ArrayList<Long> outcomeIds = keyLink.listLinks();
+			System.out.println(outcomeIds);
+		}
 		
 		//bettingoffer
-		keyLink = new B3KeyLink(Outcome.class, outcomeIds.get(0), BettingOffer.class, "outcomeId");
+		keyLink = new B3KeyLink(Outcome.class, 0, BettingOffer.class, "outcomeId");
 		ArrayList<Long> offerIds = keyLink.listLinks();
 		B3KeyEntity.load(jsonMapper, BettingOffer.class, offerIds);
 		
