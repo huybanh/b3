ad6aa5e1f123da650e759ec70ec185d6ae66aefe
Huy Banh <huy.banh@gmail.com>
2016-05-11 14:18:02 +0700

    removed unused classes

--- pushclient/src/main/java/com/betbrain/b3/report/EntityQuery.java
+++ pushclient/src/main/java/com/betbrain/b3/report/EntityQuery.java
@@ -3,14 +3,14 @@ package com.betbrain.b3.report;
 import com.betbrain.b3.data.B3KeyEntity;
 import com.betbrain.b3.data.DynamoWorker;
 import com.betbrain.b3.pushclient.JsonMapper;
-import com.betbrain.sepc.connector.sportsmodel.Outcome;
+import com.betbrain.sepc.connector.sportsmodel.*;
 
 public class EntityQuery {
 
 	public static void main(String[] args) {
 		DynamoWorker.initBundleCurrent();
 		JsonMapper mapper = new JsonMapper();
-		B3KeyEntity keyEntity = new B3KeyEntity(Outcome.class, 3020356094L);
+		B3KeyEntity keyEntity = new B3KeyEntity(Event.class, IDs.EVENT_PREMIERLEAGUE);
 		keyEntity.load(mapper);
 	}
 
