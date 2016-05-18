d8ffe6839ce1dddddbc084aab939fe450530d2c8
Huy Banh <huy.banh@gmail.com>
2016-05-18 12:26:11 +0700

    new version

--- pushclient/src/main/java/com/betbrain/b3/report/EventInfoQuery.java
+++ pushclient/src/main/java/com/betbrain/b3/report/EventInfoQuery.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 import com.betbrain.b3.data.B3KeyEventInfo;
 import com.betbrain.b3.data.DynamoWorker;
+import com.betbrain.b3.data.RevisionedEntity;
+import com.betbrain.b3.model.B3Entity;
 import com.betbrain.b3.pushclient.JsonMapper;
-import com.betbrain.sepc.connector.sportsmodel.EventInfo;
 
 public class EventInfoQuery {
 	
@@ -14,20 +15,12 @@ public class EventInfoQuery {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		
-		//DynamoWorker.initBundleCurrent();
-		DynamoWorker.initBundleByStatus("SPRINT2");
-		System.out.println("Scores");
-		B3KeyEventInfo scoreKey = new B3KeyEventInfo(217562668L, 1L, null);
-		ArrayList<EventInfo> scores = (ArrayList<EventInfo>) scoreKey.listEntities(true, mapper);
-		for (EventInfo one : scores) {
-			System.out.println(one);
-		}
-		
-		System.out.println("Match statuses");
-		B3KeyEventInfo statusKey = new B3KeyEventInfo(217562668L, 92L, null);
-		ArrayList<EventInfo> statuses = (ArrayList<EventInfo>) statusKey.listEntities(true, mapper);
-		for (EventInfo one : statuses) {
-			System.out.println(one);
+		DynamoWorker.initBundleCurrent();
+		System.out.println("info");
+		B3KeyEventInfo infoKey = new B3KeyEventInfo(219900664L, null, null, null);
+		ArrayList<RevisionedEntity<?>> infos = (ArrayList<RevisionedEntity<?>>) infoKey.listEntities(true, mapper);
+		for (RevisionedEntity<?> one : infos) {
+			System.out.println(((B3Entity<?>) one.b3entity).entity);
 		}
 	}
 }
